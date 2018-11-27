a3e1e4574e2cb12cf1f57cd61dd6e60f485e8af7
Dimitri comenda <comenda.dimitri007@gmail.com>
2018-11-27 12:02:17 +0100

    Réfèrencement élèment graphique

--- app/src/main/java/com/comenda/dimitri/topquiz/MainActivity.java
+++ app/src/main/java/com/comenda/dimitri/topquiz/MainActivity.java
@@ -2,12 +2,23 @@ package com.comenda.dimitri.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWelcomeText;
+    private EditText mNameInput;
+    private Button mPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWelcomeText = (TextView) findViewById(R.id.activity_main_welcome_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
     }
 }
