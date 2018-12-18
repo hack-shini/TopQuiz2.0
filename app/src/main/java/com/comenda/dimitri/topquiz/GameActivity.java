15b4d0ed03eb3c6ed05d9de66950824827c7ba13
Dimitri comenda <comenda.dimitri007@gmail.com>
2018-12-18 09:14:52 +0100

    Test de l'émulateur (corrigé car gros problème) + réation seconde activité + référencement graphique des élèments

---
+++ app/src/main/java/com/comenda/dimitri/topquiz/GameActivity.java
@@ -0,0 +1,28 @@
+package com.comenda.dimitri.topquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    private TextView mQuestionText;
+    private Button mAnswer1Btn;
+    private Button mAnswer2Btn;
+    private Button mAnswer3Btn;
+    private Button mAnswer4Btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        mQuestionText = (TextView) findViewById(R.id.activity_game_question1_text);
+        mAnswer1Btn = (Button) findViewById(R.id.activity_game_answer1_btn);
+        mAnswer2Btn = (Button) findViewById(R.id.activity_game_answer2_btn);
+        mAnswer3Btn = (Button) findViewById(R.id.activity_game_answer3_btn);
+        mAnswer4Btn = (Button) findViewById(R.id.activity_game_answer4_btn);
+
+    }
+}
--- app/src/main/java/com/comenda/dimitri/topquiz/MainActivity.java
+++ app/src/main/java/com/comenda/dimitri/topquiz/MainActivity.java
@@ -1,7 +1,11 @@
 package com.comenda.dimitri.topquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,5 +24,35 @@ public class MainActivity extends AppCompatActivity {
         mWelcomeText = (TextView) findViewById(R.id.activity_main_welcome_txt);
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
         mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
+
+        mPlayButton.setEnabled(false);
+
+
+        mNameInput.addTextChangedListener(new TextWatcher()  {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPlayButton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameActivity = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameActivity);
+
+            }
+        });
+
     }
 }
